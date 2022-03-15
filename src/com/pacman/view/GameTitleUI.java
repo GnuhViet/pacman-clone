2034976e128ccf6d6a11d69a394ba8b2db5a5918
GNUH <34486659+nvh202sua@users.noreply.github.com>
2022-03-15 17:11:36 +0700

    update 0.1

---
+++ src/com/pacman/view/GameTitleUI.java
@@ -0,0 +1,196 @@
+package com.pacman.view;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.*;
+
+public class GameTitleUI {
+    private static final String GAME_TITLE = "PACMAN";
+
+    private static final int SCREEN_WIDTH = 600;
+    private static final int SCREEN_HEIGHT = 750;
+
+    private JFrame window;
+    private ImagePanel titleUI;
+    private JPanel gameUI;
+    private Container con;
+
+    public GameTitleUI() {
+        initFrame();
+        initTileUI();
+    }
+
+    private void initFrame() {
+        window = new JFrame();
+        window.setTitle(GAME_TITLE);
+        window.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+
+    private void initTileUI() {
+        titleUI = new ImagePanel("src/com/pacman/res/title-background.jpg");
+        con = window.getContentPane();
+
+        // Title panel config
+        titleUI.setLayout(new BoxLayout(titleUI, BoxLayout.Y_AXIS));
+
+        // sub title panel
+        JPanel menuPanel = new JPanel();
+        JPanel logoPanel = new JPanel();
+
+        // Logo panel config
+        JLabel logo = new JLabel();
+        logo.setIcon(new ImageIcon("src/com/pacman/res/menu-logo.png"));
+        logoPanel.add(logo);
+        logoPanel.setBorder(new EmptyBorder(40,0,0,0));
+        logoPanel.setOpaque(false);
+
+        // Menu panel config
+        menuPanel.setAlignmentY(Component.CENTER_ALIGNMENT);
+        menuPanel.setPreferredSize(new Dimension(300,290));
+        menuPanel.setMaximumSize(new Dimension(300, 290));
+
+        // can le ben duoi
+        menuPanel.setBorder(BorderFactory.createEmptyBorder(0,0,20,0));
+        menuPanel.setLayout(new GridBagLayout());
+        menuPanel.setOpaque(false);
+
+        MenuButton startBtn = new MenuButton("StartButton.png");
+        MenuButton continueBtn = new MenuButton("ContinueButton.png");
+        MenuButton scoreBtn = new MenuButton("ScoreButton.png");
+        MenuButton optionBtn = new MenuButton("OptionsButton.png");
+        MenuButton quitBtn = new MenuButton("QuitButton.png");
+
+        //add button to menu panel
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(0,0,5,0);
+        c.gridy = 0;
+        menuPanel.add(startBtn, c);
+        c.gridy = 1;
+        menuPanel.add(continueBtn, c);
+        c.gridy = 2;
+        menuPanel.add(scoreBtn, c);
+        c.gridy = 3;
+        menuPanel.add(optionBtn, c);
+        c.gridy = 4;
+        menuPanel.add(quitBtn, c);
+
+        // add to title ui
+        titleUI.add(logoPanel);
+        titleUI.add(menuPanel);
+
+        // add to frame
+        con.add(titleUI);
+    }
+
+    private void initGameUI() {
+
+        titleUI.setVisible(false);
+
+        gameUI = new JPanel();
+        con.add(gameUI);
+
+        Label gameLb = new Label("DAY LA GAME");
+
+        gameUI.add(gameLb);
+        gameLb.setVisible(true);
+    }
+
+    // image button
+    private class MenuButton extends JLabel implements MouseListener {
+        private static final String iconFolPath = "src\\com\\pacman\\res\\MenuButton\\";
+        String buttonName;
+        String iconName;
+        String activeIconName;
+
+        ImageIcon norIcon;
+        ImageIcon actIcon;
+
+        public MenuButton(String iconName) {
+            super();
+            this.buttonName = iconName;
+            this.iconName = iconFolPath + iconName;
+            this.activeIconName = iconFolPath + "Active" + iconName;
+
+            norIcon = new ImageIcon(this.iconName);
+            actIcon = new ImageIcon(activeIconName);
+
+            this.setIcon(norIcon);
+            addMouseListener(this);
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+            if (buttonName.equals("StartButton.png")) {
+                initGameUI();
+                return;
+            }
+
+            if (buttonName.equals("ContinueButton.png")) {
+                // TODO lam menu continue
+                return;
+            }
+
+            if (buttonName.equals("ScoreButton.png")) {
+                return;
+            }
+
+            if (buttonName.equals("OptionsButton.png")) {
+
+                return;
+            }
+
+            // quit button
+            if (buttonName.equals("QuitButton.png")) {
+                System.exit(0);
+            }
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            this.setIcon(actIcon);
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            this.setIcon(norIcon);
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+        }
+    }
+
+    // Background cho main menu
+    private class ImagePanel extends JPanel {
+
+        private Image img;
+
+        public ImagePanel(String img) {
+            this(new ImageIcon(img).getImage());
+        }
+
+        public ImagePanel(Image img) {
+            this.img = img;
+            Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+            setPreferredSize(size);
+            setMinimumSize(size);
+            setMaximumSize(size);
+            setSize(size);
+            setLayout(null);
+        }
+
+        public void paintComponent(Graphics g) {
+            g.drawImage(img, 0, 0, null);
+        }
+    }
+}
